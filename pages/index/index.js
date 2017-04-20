7be414a356ed85676260b10c4f981cfc533abdd4
zanjs <root@zanjs.com>
2017-04-20 11:40:33 +0800

    adddot :question:

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-var tcity = require("../../utils/citys.js");
+var tcity = require("../../utils/citys.js")
 
 var app = getApp()
 Page({
@@ -20,49 +20,49 @@ Page({
     var val = e.detail.value
     var t = this.data.values;
     var cityData = this.data.cityData;
-    
-    if(val[0] != t[0]){
+
+    if (val[0] != t[0]) {
       console.log('province no ');
       const citys = [];
       const countys = [];
 
-      for (let i = 0 ; i < cityData[val[0]].sub.length; i++) {
+      for (let i = 0; i < cityData[val[0]].sub.length; i++) {
         citys.push(cityData[val[0]].sub[i].name)
       }
-      for (let i = 0 ; i < cityData[val[0]].sub[0].sub.length; i++) {
+      for (let i = 0; i < cityData[val[0]].sub[0].sub.length; i++) {
         countys.push(cityData[val[0]].sub[0].sub[i].name)
       }
 
       this.setData({
         province: this.data.provinces[val[0]],
         city: cityData[val[0]].sub[0].name,
-        citys:citys,
+        citys: citys,
         county: cityData[val[0]].sub[0].sub[0].name,
-        countys:countys,
+        countys: countys,
         values: val,
-        value:[val[0],0,0]
+        value: [val[0], 0, 0]
       })
-      
+
       return;
     }
-    if(val[1] != t[1]){
+    if (val[1] != t[1]) {
       console.log('city no');
       const countys = [];
 
-      for (let i = 0 ; i < cityData[val[0]].sub[val[1]].sub.length; i++) {
+      for (let i = 0; i < cityData[val[0]].sub[val[1]].sub.length; i++) {
         countys.push(cityData[val[0]].sub[val[1]].sub[i].name)
       }
-      
+
       this.setData({
         city: this.data.citys[val[1]],
         county: cityData[val[0]].sub[val[1]].sub[0].name,
-        countys:countys,
+        countys: countys,
         values: val,
-        value:[val[0],val[1],0]
+        value: [val[0], val[1], 0]
       })
       return;
     }
-    if(val[2] != t[2]){
+    if (val[2] != t[2]) {
       console.log('county no');
       this.setData({
         county: this.data.countys[val[2]],
@@ -70,46 +70,59 @@ Page({
       })
       return;
     }
-    
+
 
   },
-  open:function(){
+  open: function() {
     this.setData({
-      condition:!this.data.condition
+      condition: !this.data.condition
     })
   },
-  onLoad: function () {
+  addDot: function(arr) {
+    if (arr instanceof Array) {
+      arr.map(val => {
+        if (val.fullName.length > 4) {
+          val.fullNameDot = val.fullName.slice(0, 4) + '...';
+          return val;
+        } else {
+          val.fullNameDot = val.fullName;
+          return val;
+        }
+      })
+    }
+  },
+  onLoad: function() {
     console.log("onLoad");
     var that = this;
-    
+
     tcity.init(that);
 
     var cityData = that.data.cityData;
 
-    
+
     const provinces = [];
     const citys = [];
     const countys = [];
 
-    for(let i=0;i<cityData.length;i++){
+    for (let i = 0; i < cityData.length; i++) {
       provinces.push(cityData[i].name);
     }
     console.log('省份完成');
-    for (let i = 0 ; i < cityData[0].sub.length; i++) {
+    for (let i = 0; i < cityData[0].sub.length; i++) {
       citys.push(cityData[0].sub[i].name)
     }
     console.log('city完成');
-    for (let i = 0 ; i < cityData[0].sub[0].sub.length; i++) {
+    for (let i = 0; i < cityData[0].sub[0].sub.length; i++) {
       countys.push(cityData[0].sub[0].sub[i].name)
     }
 
     that.setData({
       'provinces': provinces,
-      'citys':citys,
-      'countys':countys,
-      'province':cityData[0].name,
-      'city':cityData[0].sub[0].name,
-      'county':cityData[0].sub[0].sub[0].name
+      'citys': citys,
+      'countys': countys,
+      'province': cityData[0].name,
+      'city': cityData[0].sub[0].name,
+      'county': cityData[0].sub[0].sub[0].name
     })
     console.log('初始化完成');
 
